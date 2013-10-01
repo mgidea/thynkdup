1d3361775ed4e5447b104046f80c30d2280f8af7
Tom Corley <tomcorleycarpentry@gmail.com>
2013-10-01 13:47:55 -0400

    added simple form and first_name last_name

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-feature "user signs up", %q{
+feature "user signs up", %Q{
   As a curious user
   I want to create an account
   so that I can use thynkdup
@@ -15,13 +15,17 @@ feature "user signs up", %q{
 # -When completed I am a signed in user and I am directed to my new profile page
   scenario "user signs up succesfully" do
     email = "tom@corley.com"
-    visit new_user_registration_path
+    visit root_path
+    click_link "Sign Up"
     fill_in "Email", with: email
-    fill_in "Password", with: "password"
-
-    fill_in "Password Confirmation", with: "password"
-    click_button "Sign Up"
     save_and_open_page
+
+    fill_in("Password", with: 'password', :match => :prefer_exact)
+    fill_in("Password Confirmation", with: 'password', :match => :prefer_exact)
+    fill_in "First Name", with: "Tom"
+    fill_in "Last Name", with: "Corley"
+
+    click_button "Sign Up"
     expect(page).to have_content "Get ready to build on your ideas!"
     expect(User.find_by(email: email)).to be_present
   end
