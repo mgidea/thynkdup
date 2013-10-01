52bb0974e9393d176660fde8f7bf3117378f76dd
Tom Corley <tomcorleycarpentry@gmail.com>
2013-10-01 14:04:15 -0400

    confirmed password mismatch error

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -17,9 +17,8 @@ feature "user signs up", %Q{
     email = "tom@corley.com"
     visit root_path
     click_link "Sign Up"
-    fill_in "Email", with: email
-    save_and_open_page
 
+    fill_in "Email", with: email
     fill_in("Password", with: 'password', :match => :prefer_exact)
     fill_in("Password Confirmation", with: 'password', :match => :prefer_exact)
     fill_in "First Name", with: "Tom"
@@ -30,5 +29,28 @@ feature "user signs up", %Q{
     expect(User.find_by(email: email)).to be_present
   end
 
+  scenario "User does not sign up successfully" do
+    prev_count = User.count
+    visit root_path
+    click_link "Sign Up"
+    click_button "Sign Up"
+    expect(page).to have_content "can't be blank"
+    expect(User.count).to eql(prev_count)
+  end
+
+  scenario "User has non-matching password and password confirmation" do
+    email = "tom@corley.com"
+    visit root_path
+    click_link "Sign Up"
+    fill_in "Email", with: email
+    fill_in("Password", with: 'password', :match => :prefer_exact)
+    fill_in("Password Confirmation", with: 'different', :match => :prefer_exact)
+    fill_in "First Name", with: "Tom"
+    fill_in "Last Name", with: "Corley"
+    click_button "Sign Up"
+
+    expect(page).to have_content "doesn't match Password"
+    expect(User.find_by(email: email)).to_not be_present
+  end
 end
 
