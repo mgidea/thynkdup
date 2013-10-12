a6fd2ffe42612b979a44fbed7f0b1b39ee146136
Tom Corley <tomcorleycarpentry@gmail.com>
2013-10-12 10:46:16 -0400

    working through create note spec

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,22 @@
 class NotesController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @nutshell = Nutshell.find(params[:nutshell_id])
+    @note = @nutshell.notes.build
+  end
+
+  def create
+  end
+
+  def show
+
+  end
+
+protected
+  def note_params
+    params.require(:note).permit(:content)
+  end
 end
