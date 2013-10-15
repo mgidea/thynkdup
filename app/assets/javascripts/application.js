4107fdb1c3ac2c9a42d85232e4598e831b80002c
Tom Corley <tomcorleycarpentry@gmail.com>
2013-10-15 13:55:54 -0400

    started on layout styles

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require cocoon
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,8 @@ class User < ActiveRecord::Base
 
   validates_presence_of :first_name
   validates_presence_of :last_name
+
+  def full_name
+    first_name + " " + last_name
+  end
 end
