07a66b4a2c5d6bedbf19a0162b7f7c54159734fa
Tom Corley <tom.corley@goodmeasures.com>
2014-03-31 08:12:24 -0400

    add index on thynkups, continue testing on thynkups model.

--- db/migrate/20140328150809_change_thynkups_column.rb
+++ db/migrate/20140328150809_change_thynkups_column.rb
@@ -7,6 +7,8 @@ class ChangeThynkupsColumn < ActiveRecord::Migration
   end
 
   def down
+    remove_index :thynkups, column: [:friend_id, :thynker_id]
+
     add_column    :thynkups, :user_id, :integer
     remove_column :thynkups, :friend_id, :integer
   end
